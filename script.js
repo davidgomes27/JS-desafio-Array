fa78c80eb0c0f6d6f96033c0549c0732255323c3
dev-davidgomes <davidsmithcrlh@outlook.com>
2021-05-21 02:09:06 -0300

    adicionando desafio Array logica js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+let comidas = ["Pizza","empadão","lazanha","torta","strogonoff"]
+
+comidas.unshift("cachorro-quente")
+comidas.pop()
+comidas.push("macarrão","bife")
+comidas.shift()
+console.log(comidas)
+
+let numeros =[7,5,6,3,8,9,2,1,4]
+
+numeros.sort()
+
+console.log(numeros)
